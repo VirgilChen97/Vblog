5beabb2c980b753d3ee3d82bd5b25bf99f3920ee
Virgil Chen <virgilchen97@gmail.com>
2020-07-15 17:02:02 -0500

    remove unecessary files

--- src/main/java/com/cyf/myblogserver/data/AuthenticationRequest.java
+++
@@ -1,9 +0,0 @@
-package com.cyf.myblogserver.data;
-
-import lombok.Data;
-
-@Data
-public class AuthenticationRequest {
-    private String username;
-    private String password;
-}
--- src/main/java/com/cyf/myblogserver/exception/AuthenticationFailedException.java
+++
@@ -1,7 +0,0 @@
-package com.cyf.myblogserver.exception;
-
-public class AuthenticationFailedException extends Exception{
-    public AuthenticationFailedException() {
-        super();
-    }
-}
